d0003d3b2ae62c6ec5dfb9b9d4a1f5f7de0c72b5
john doe <john@tekst.com>
2016-11-08 14:13:12 +0100

    opgave 1-3

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Consoleminiopgaver
+{
+    public class miniopgave
+    {
+        public static void Main()
+        {
+            Opgave1();
+            Opgave2();
+            Opgave3();
+            
+        }
+
+        //Her er opgave 1
+        public static void Opgave1()
+        {
+            Debug.WriteLine("Opgave 1");
+            Debug.WriteLine("Hello world");
+            Debug.WriteLine("Tommy Grønvold");
+        }
+
+        //Her er opgave 2
+        public static void Opgave2()
+        {
+            Debug.WriteLine("opgave 2");
+            Debug.WriteLine(12 + 13);
+        }   
+        //Her er opgave 3
+        public static void Opgave3()
+        {
+            Debug.WriteLine("opgave 3");
+            Debug.WriteLine(24 / 5);
+        //Her er opgave 4   
+            
+
+        
+        }
+    }
+}
